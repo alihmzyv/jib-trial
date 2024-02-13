602854a93de16b66c51bd912e4aa02eda7407a1d
Ali Hamzayev <alihmzyv@gmail.com>
2024-02-13 22:17:57 +0400

    Initial commit.

---
+++ src/main/java/com/alihmzyv/jibtrial/JibTrialApplication.java
@@ -0,0 +1,13 @@
+package com.alihmzyv.jibtrial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibTrialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JibTrialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alihmzyv/jibtrial/JibTrialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alihmzyv.jibtrial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibTrialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
